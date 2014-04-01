de435b728742e073a4e027446a8a958b7ca5e571
sue7ga <sue77ga@gmail.com>
2014-03-31 21:34:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::NHK::API - It's new $module
+
+# SYNOPSIS
+
+    use WWW::NHK::API;
+
+# DESCRIPTION
+
+WWW::NHK::API is ...
+
+# LICENSE
+
+Copyright (C) sue7ga.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sue7ga <sue77ga@gmail.com>
---
+++ lib/WWW/NHK/API.pm
@@ -0,0 +1,39 @@
+package WWW::NHK::API;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::NHK::API - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::NHK::API;
+
+=head1 DESCRIPTION
+
+WWW::NHK::API is ...
+
+=head1 LICENSE
+
+Copyright (C) sue7ga.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sue7ga E<lt>sue77ga@gmail.comE<gt>
+
+=cut
+
